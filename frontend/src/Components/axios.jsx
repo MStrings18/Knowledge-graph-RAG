4fb137a3b627dd0312ad303ba275c5576f14dfa5
Manan Verma <113776068+MStrings18@users.noreply.github.com>
2025-09-22 00:36:23 +0530

    Update axios.jsx

--- frontend/src/Components/axios.jsx
+++ frontend/src/Components/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.82.77.192:8000", // 🔹 Replace with your backend URL
+  baseURL: "http://34.66.255.153", // 🔹 Replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
